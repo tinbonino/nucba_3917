c33e8b8a3a8b4d073369f80ccbb63e4a39f99f1f
martin <tinbonino@gmail.com>
2024-09-03 20:54:51 -0300

    Clase-01

---
+++ clase-01/script.js
@@ -0,0 +1,25 @@
+const numeroEntero = 10;
+const numeroDecimal = 3.14;
+const cadenaTexto = "Bienvenidos a backend";
+const boolean = true;
+const array= ["Leandro", "Tomas","Franquito", "Horacio"];
+
+const objeto = {
+    nombre: "Nachito",
+    edad: 56,
+    ciudad: "San Jorge"
+};
+
+const funcion = function() {
+    console.log("Hola soy una función en backend")
+}
+
+
+console.log("Numero entero", numeroEntero);
+console.log("Numero decimal", numeroDecimal);
+console.log("Cadena de texto",cadenaTexto);
+console.log("Booleano", boolean);
+console.log("Array", array);
+console.log("Objeto", objeto);
+console.log("Función", funcion());
+
