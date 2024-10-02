50c253ad0eebe039fd3baa26903b97a203bab5de
martin <tinbonino@gmail.com>
2024-10-01 21:23:44 -0300

    clase 9

---
+++ clase-09/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import { getPokemonById, getSimplePokemonById,getMultiplePokemon } from "./controllers/pokemon";
+
+const app = express();
+const PORT = 8080;
+
+// preparamos el middleware
+
+app.use(express.json())
+
+// Creamos las rutas
+
+app.get("/pokemon/full/:id",getPokemonById);
+
+app.get("/pokemon/simple/:id",getSimplePokemonById);
+
+app.get("/pokemon/multiple",getMultiplePokemon);
+
+// Inicializamos el servidor
+
+app.listen(PORT,() => {
+  console.log("Servidor funcionando en el puerto",PORT);
+})
+
---
+++ clase-09/pasos.md
@@ -0,0 +1,13 @@
+# Pasos
+
+- npm init -y
+- npm i express
+- npm i typescript
+- npm i axios
+- tsc --init
+- npm i nodemon
+- Configurar el outdir en tsconfig.ts
+- "outDir":"./dist"
+
+## Para que reconozca el import de los types de express
+- npm i --save-dev @types/express
